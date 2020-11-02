8ba1428db006642ad305a35e14b145c328fed7ae
abdulrahman <a.alharasis@arabot.io>
2020-11-02 18:06:14 +0000

    add code in files types: .java,.json remove code in files types: .java,.json

--- content.java
+++ content.java
@@ -99,6 +99,6 @@ public class Cwvbhx {
         System.out.println("xkagi");
         System.out.println("pwhup");
         System.out.println("ttkma");
-        System.out.println("ijhtn");
+        System.out.println("qwile");
     }
 }
