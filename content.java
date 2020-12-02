687629dd3268f87dfc92aac49d49de7f855e24c0
abdulrahman <a.alharasis@arabot.io>
2020-12-02 11:58:55 +0000

    add code in files types: .java,.yml remove code in files types: .java

--- content.java
+++ content.java
@@ -23,6 +23,6 @@ public class Cwvbhx {
         System.out.println("matgb");
         System.out.println("wkpdr");
         System.out.println("thexu");
-        System.out.println("encum");
+        System.out.println("bqkrq");
     }
 }
