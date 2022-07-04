734deb8974d79d1f4d6073720d424b520981bd1b
abdulrahman <a.alharasis@arabot.io>
2022-07-04 09:23:59 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -174,6 +174,6 @@ public class Cwvbhx {
         System.out.println("ixtut");
         System.out.println("hmued");
         System.out.println("aqexw");
-        System.out.println("chgqh");
+        System.out.println("ivayv");
     }
 }
