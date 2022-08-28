c3a57fb510cbe1de067ca343d66d11c7a8d58e21
abdulrahman <a.alharasis@arabot.io>
2022-08-28 20:37:07 +0000

    add code in files types: remove code in files types: .java

--- content.java
+++ content.java
@@ -286,6 +286,5 @@ public class Cwvbhx {
         System.out.println("ktwan");
         System.out.println("glahi");
         System.out.println("cerbd");
-        System.out.println("qldke");
     }
 }
