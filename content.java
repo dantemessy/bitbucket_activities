9338892b9d92110969111945aec2b4292adcbca1
abdulrahman <a.alharasis@arabot.io>
2023-12-27 11:10:35 +0000

    add code in files types: .xml,.java remove code in files types: .xml

--- content.java
+++ content.java
@@ -393,5 +393,6 @@ public class Cwvbhx {
         System.out.println("fofjb");
         System.out.println("xnssc");
         System.out.println("yyoey");
+        System.out.println("plull");
     }
 }
